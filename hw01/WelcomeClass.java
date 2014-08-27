32b2c6636264e0e460a28514323be044494e455e
alecdouglass <ald218@lehigh.edu>
2014-08-27 01:04:10 +0000

    Finished WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Alec Douglass
+//Welcome Class
+
+public class WelcomeClass{
+    
+    public static void main(String[] args)  {
+        
+//Out put with network ID and small statement
+        System.out.println("    ------------- ");
+        System.out.println("    |  WELCOME  | ");
+        System.out.println("    ------------- ");
+        System.out.println("   ^  ^  ^  ^  ^  ^ ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println(" |-a--l--d--2--1--8-| ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("   v  v  v  v  v  v ");
+        System.out.println("I like to read books");
+        System.out.println(" The End ");
+        
+        //
+    }
+}
