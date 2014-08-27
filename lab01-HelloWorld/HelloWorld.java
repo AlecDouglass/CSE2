96adca36b77114d02ba71f1e963b59a9d2cec8a4
alecdouglass <ald218@lehigh.edu>
2014-08-27 00:22:02 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+// Alec Douglass
+//Hello World Java
+
+//define class
+public class HelloWorld{
+    
+//main method
+  public static void main(String[] args) {
+ 
+//   statement with box
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("##                     ##");
+    System.out.println("##                     ##");
+    System.out.println("##  Hello World 2014!  ##");
+    System.out.println("##                     ##");
+    System.out.println("##                     ##");
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("test");
+
+//      test
+  }
+}
