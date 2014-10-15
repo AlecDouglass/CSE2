d11c90306086902907c813b3284fd0787fb18e19
alecdouglass <ald218@lehigh.edu>
2014-10-15 16:15:32 +0000

    Everything

--- lab07/LoopTheLoop.java
+++ lab07/LoopTheLoop.java
@@ -10,13 +10,56 @@ public class LoopTheLoop{
         int nStars = 10;
         int nTest = 0;
         char star = '*';
-        while (nTest<nStars){
-            nTest++;
-            System.out.print(star);
-            if (nTest==10){
-                break;
-            }   
-        }//end first while
-        
+        int line = 0;
+        Scanner myScanner = new Scanner (System.in);
+        System.out.print("Enter an int between 1 and 15: ");
+        if (myScanner.hasNextInt()){
+            int input = myScanner.nextInt();
+            while (nTest<nStars){
+                nTest++;
+                System.out.print(star);
+                if (nTest==10){
+                    System.out.println(" ");
+                }   
+                while (line<input){
+                    line++;
+                    switch (line) {
+                        case 1: System.out.print("");
+                            System.out.println(" "); break;
+                        case 2: System.out.print("**");
+                            System.out.println(" "); break;
+                        case 3: System.out.print("***");
+                            System.out.println(" "); break;
+                        case 4: System.out.print("****");
+                            System.out.println(" "); break;
+                        case 5: System.out.print("*****");
+                            System.out.println(" "); break;
+                        case 6: System.out.print("******");
+                            System.out.println(" "); break;
+                        case 7: System.out.print("*******");
+                            System.out.println(" "); break;
+                        case 8: System.out.print("********");
+                            System.out.println(" "); break;
+                        case 9: System.out.print("*********");
+                            System.out.println(" "); break;
+                        case 10: System.out.print("**********");
+                            System.out.println(" "); break;
+                        case 11: System.out.print("***********");
+                            System.out.println(" "); break;
+                        case 12: System.out.print("************");
+                            System.out.println(" "); break;
+                        case 13: System.out.print("*************");
+                            System.out.println(" "); break;
+                        case 14: System.out.print("**************");
+                            System.out.println(" "); break;
+                        case 15: System.out.print("***************");
+                            System.out.println(" "); break;
+                    }
+                }
+            }//end first while
+        }
+        else{
+            System.out.println("Invalid input");
+        }
     }//end method
 }//end class
